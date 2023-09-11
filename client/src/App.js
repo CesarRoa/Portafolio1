f3b067f3cbe7bf7828f089714e70fe16064fe08f
Cesar Roa <kezar1@gmail.com>
2023-09-11 01:20:41 -0400

    CesaRoa: feat: add axios and endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const App =() => {
-  useEffect(()=>{
-    fetch('/hello')
-    .then(res => res.json())
-    .then(data => console.log(data))
-  })
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'https://footapi7.p.rapidapi.com/api/rankings/uefa/countries',
+            headers: {
+                'X-RapidAPI-Key': '056d2999cemshb3444f3c853b8a3p1ed62cjsn076f8982e203',
+                'X-RapidAPI-Host': 'footapi7.p.rapidapi.com'
+            }
+        };
+
+        axios.request(options)
+            .then(response => {
+                setData(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    return (
+        <div>
+            {/* Render your data here */}
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    );
 }
 
 export default App;
--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,32 @@
-const router = require('express').Router();
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
 
-router.get('/hello', (req, res)=>{
-    return res.status(200).json({
-        status: 200,
-        message: "hello test"
-    });
+router.get("/hello", (req, res) => {
+  return res.status(200).json({
+    status: 200,
+    message: "hello test",
+  });
 });
+
+router.get("/uefa-rankings", async (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://footapi7.p.rapidapi.com/api/rankings/uefa/countries',
+        headers: {
+            'X-RapidAPI-Key': '056d2999cemshb3444f3c853b8a3p1ed62cjsn076f8982e203',
+            'X-RapidAPI-Host': 'footapi7.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to fetch UEFA rankings" });
+    }
+});
+
 
 module.exports = router;
